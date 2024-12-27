4c53965cc739a4d3fe4e5fb4017a2a5a7eb28dda
divya-garg312 <dgarg2609@gmail.com>
2024-12-27 16:05:14 +0530

    Add files via upload

---
+++ loadHeader.js
@@ -0,0 +1,28 @@
+function loadHeader(){
+    fetch('header.html')
+    .then(response=>response.text())
+    .then(data=>{
+       
+        document.getElementById("nav").innerHTML=data;
+    })
+}
+
+function navToPage() {
+    let selectedValue = document.getElementById("destinations-select").value;
+    if (selectedValue) {
+        window.location.href = selectedValue; 
+    }
+}
+
+function loadFooter(){
+    fetch('footer.html')
+    .then(response=>response.text())
+    .then(data=>{
+        document.getElementById("footer").innerHTML=data;
+    })
+}
+
+
+
+document.addEventListener("DOMContentLoaded",loadHeader);
+document.addEventListener("DOMContentLoaded",loadFooter);
