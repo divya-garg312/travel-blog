fc4ae4ae3db4c55e9b3bced2ac7086a20fa1b857
divya-garg312 <dgarg2609@gmail.com>
2024-12-28 12:40:49 +0530

    Add files via upload

--- loadHeader.js
+++ loadHeader.js
@@ -1,28 +1,28 @@
-function loadHeader(){
+function loadHeader() {
     fetch('header.html')
-    .then(response=>response.text())
-    .then(data=>{
-       
-        document.getElementById("nav").innerHTML=data;
-    })
+        .then(response => response.text())
+        .then(data => {
+
+            document.getElementById("nav").innerHTML = data;
+        })
 }
 
 function navToPage() {
     let selectedValue = document.getElementById("destinations-select").value;
     if (selectedValue) {
-        window.location.href = selectedValue; 
+        window.location.href = selectedValue;
     }
 }
 
-function loadFooter(){
+function loadFooter() {
     fetch('footer.html')
-    .then(response=>response.text())
-    .then(data=>{
-        document.getElementById("footer").innerHTML=data;
-    })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
 }
 
 
 
-document.addEventListener("DOMContentLoaded",loadHeader);
-document.addEventListener("DOMContentLoaded",loadFooter);
+document.addEventListener("DOMContentLoaded", loadHeader);
+document.addEventListener("DOMContentLoaded", loadFooter);
